b1f1b3ada33ffead376ef809cc2d7dc93e5cbfe1
ClaudioDenner <78771522+ClaudioDenner@users.noreply.github.com>
2023-02-28 00:28:01 -0300

    "att"

---
+++ js/scripts/scroll.js
@@ -0,0 +1,12 @@
+
+        ScrollReveal({ reset: true });
+        ScrollReveal().reveal('.sobre-mim', { delay: 200 });
+        ScrollReveal().reveal('.educacao', { delay: 200 });
+        ScrollReveal().reveal('.certificacoes', { delay: 200 });
+        ScrollReveal().reveal('.skills', { delay: 200 });
+        ScrollReveal().reveal('.projetos', { delay: 200 });
+
+
+
+
+        
