af4423302ab339e5d8a8991977a755e08f63ab29
ClaudioDenner <78771522+ClaudioDenner@users.noreply.github.com>
2023-03-01 20:56:22 -0300

    "80"

---
+++ js/scripts/carousel.js
@@ -0,0 +1,11 @@
+var splide = new Splide( '.splide',{
+    type    : 'loop',
+    autoplay: 'play',
+    perPage : 3,
+    width: '95%'
+  }  );
+
+
+
+
+splide.mount();
--- js/scripts/scroll.js
+++ js/scripts/scroll.js
@@ -1,10 +1,23 @@
 
         ScrollReveal({ reset: true });
-        ScrollReveal().reveal('.sobre-mim', { delay: 200 });
+        ScrollReveal().reveal('.sobre-mim', { delay: 10 });
+        ScrollReveal().reveal('.sobre-mim img', { delay: 400 });
+        ScrollReveal().reveal('.sobre-mim div', { delay: 600 });
+        
+
+
+
         ScrollReveal().reveal('.educacao', { delay: 200 });
+        ScrollReveal().reveal('.items-list', { delay: 400 });
+
         ScrollReveal().reveal('.certificacoes', { delay: 200 });
-        ScrollReveal().reveal('.skills', { delay: 200 });
-        ScrollReveal().reveal('.projetos', { delay: 200 });
+        ScrollReveal().reveal('.certificacoes li', { delay: 400 });
+
+
+        ScrollReveal().reveal('.projetos main article', { delay: 200 });
+
+
+
 
 
 
